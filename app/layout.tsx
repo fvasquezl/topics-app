4be4e28c718edaf161c12e1a251b2c4323a02e8d
Faustino Vasquez <vasquez.limon@gmail.com>
2023-07-02 16:07:15 -0700

    adding components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-3xl mx-auto p-4">
+          <Navbar />
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ components/TopicsList.jsx
@@ -0,0 +1,9 @@
+
+const TopicsList = props => {
+    return (
+        <h1>Topics</h1>
+    )
+}
+
+
+export default TopicsList
